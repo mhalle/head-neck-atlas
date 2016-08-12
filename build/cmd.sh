56ba9e5389532f76b45e7d2d01d85ca87f23f93d
Michael Halle <mhalle@bwh.harvard.edu>
2016-08-12 04:44:48 -0400

    added build

---
+++ build/cmd.sh
@@ -0,0 +1,7 @@
+python ../../tools/hawg/mrmlToHawg.py \
+    --labelfile labels/HN-Atlas-labels.nrrd \
+    --modeldir models \
+    --bgimage grayscale/Osirix-Manix-255-res.nrrd \
+    --rooturl '' \
+    --atlasname "Head and Neck Atlas" \
+    mrml/main.mrml 
